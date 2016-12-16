68fd9d01721dcab3a8485f6467c11f15f1f4cde1
Intervencion <janraven55@gmail.com>
2016-12-16 06:46:04 +0100

    Stuff in testing

--- OPTC-DB/Testing/barcos.js
+++ OPTC-DB/Testing/barcos.js
@@ -1,7 +1,5 @@
 module.exports = [
     [ "Dinghy", "STR", "Fighter", 2, 1, 5, 0, 5, 163, 42, 15, 8, 134, 68, 15, 1 ],
-    [ "Monkey D. Luffy Gum-Gum Pistol", "STR", "Fighter", 3, 4, 6, 0, 20, 26184, 130, 76, 14, 510, 249, 49, 0.7 ],
-    [ "Monkey D. Luffy Gum-Gum Bazooka", "STR", "Fighter", 4, 7, 6, 2, 35, 136167, 404, 227, 39, 975, 522, 105, 1 ],
     [ "Monkey D. Luffy Gear 2", "STR", [ "Fighter", "Free Spirit" ], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
     ];
     
