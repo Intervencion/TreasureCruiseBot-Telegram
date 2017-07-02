c9c40866e84759f0f23783d239f7c4c4f882e319
ZampoConco <antonio.zampino@hotmail.com>
2017-07-02 23:22:44 +0200

    Update help.js
    Hey Jan, It's Zampo. This is the Italian version of the bot. You should really update the news (btw, I translated them as well ahahah)

--- optcdb_bot/bots/gem/bin/utils/help.js
+++ optcdb_bot/bots/gem/bin/utils/help.js
@@ -6,46 +6,46 @@ var base = path.join(__dirname, '../..');
 module.status = require(path.join(base, 'bin/utils/status.js'));
 
 function getHelpText() {
-  var response = '<b>Inline Mode:</b>\n\n';
-  response += 'Use it in any chat.\n';
-  response += '1. Type @TreasureCruiseBot followed by keywords\n';
-  response += '2. Tap on a search result to get detailed information\n\n';
-  response += '<b>Command Mode:</b>\n\n';
-  response += 'Use it in groups.\n';
-  response += '1. Send /search to search for a unit\n';
-  response += '2. Send your keywords\n';
-  response += '3. Tap on a search result to get detailed information\n\n';
-  response += 'Talk to it directly.\n';
-  response += '1. Send your keywords\n';
-  response += '2. Tap on a search result to get detailed information\n\n';
-  response += '<b>Additional commands:</b>\n';
-  response += '/supergroup - join our crew\n';
-  response += '/inline - inline mode explained\n';
-  response += '/command - command mode explained\n';
-  response += '/rate - rate this bot on @Storebot\n';
-  response += '/notice - check the developer notice\n';
-  response += '/random - get a random unit\n';
-  response += '/bonus - a link to the bonus table\n';
-  response += '/drops - a link to the drops page\n';
-  response += '/github - a link to the Sourcecode\n';
-  response += '/help - see this\n\n';
-  response += '<b>Have fun!</b>\n';
-  response += 'Developed by trashbytes\n';
-  response += 'Maintained by @Intervencion\n\n';
-/*  response += '<b>Note:</b> This bot is under heavy development so bugs may appear every now and then.\n\n';*/
+  var response = '<b>Modalità inline:</b>\n\n';
+  response += 'Usala in qualsiasi chat.\n';
+  response += '1. Scrivi @TreasureCruiseBot seguito dalle parole chiave\n';
+  response += '2. Tocca su uno dei risultati per informazioni dettagliate\n\n';
+  response += '<b>Modalità di comandi:</b>\n\n';
+  response += 'Usala nei gruppi.\n';
+  response += '1. Invia /search per cercare un\'unità \n';
+  response += '2. Invia le tue parole chiave\n';
+  response += '3. Tocca su uno dei risultati per informazioni dettagliate\n\n';
+  response += 'Parla al bot direttamente.\n';
+  response += '1. Invia le tue parole chiave\n';
+  response += '2. Tocca su uno dei risultati per informazioni dettagliate\n\n';
+  response += '<b>Comandi extra:</b>\n';
+  response += '/supergroup - Unisciti alla nostra ciurma!\n';
+  response += '/inline - Qui troverai le istruzioni per usare la modalità inline\n';
+  response += '/command - Qui troverai le istruzioni per usare la modalità di comando\n';
+  response += '/rate - Dai un voto a questo bot su @Storebot\n';
+  response += '/notice - Controlla le notizie degli sviluppatori\n';
+  response += '/random - Ottieni le informazioni su un\'unità a caso \n';
+  response += '/bonus - un link per la tavola dei bonus\n';
+  response += '/drops - un link per la pagina dei drop\n';
+  response += '/github - un link per il codice sorgente\n';
+  response += '/help - visualizza questa scheda\n\n';
+  response += '<b>Divertiti!</b>\n';
+  response += 'Sviluppato da trashbytes\n';
+  response += 'Mantenuto da @Intervencion\n\n';
+/*  response += '<b>Nota:</b> Questo bot è costantemente sotto sviluppo, potrebbe comparire un bug ogni tanto.\n\n';*/
   response += 'Database: 2016-12-18\n';
   return response;
 }
 
 function getStart(cmd, arg, message) {
   module.status.addRequest('start');
-  var response = '<b>W E L C O M E</b>\n\n' + getHelpText();
+  var response = '<b>B E N V E N U T O</b>\n\n' + getHelpText();
   return {
     text: response,
     reply_markup: {
       inline_keyboard: [
         [{
-          text: 'Get started!',
+          text: 'Avvio!',
           switch_inline_query: ''
         }]
       ]
@@ -56,13 +56,13 @@ function getStart(cmd, arg, message) {
 
 function getHelp(cmd, arg, message) {
   module.status.addRequest('help');
-  var response = '<b>H O W   T O   U S E</b>\n\n' + getHelpText();
+  var response = '<b>I S T R U Z I O N I</b>\n\n' + getHelpText();
   return {
     text: response,
     reply_markup: {
       inline_keyboard: [
         [{
-          text: 'Get started!',
+          text: 'Avvio!',
           switch_inline_query: ''
         }]
       ]
@@ -73,20 +73,20 @@ function getHelp(cmd, arg, message) {
 
 function getInline(cmd, arg, message) {
   module.status.addRequest('help');
-  var response = '<b>H O W   T O   U S E</b>\n\n';
-  response += '<b>Inline Mode:</b>\n';
-  response += 'The inline mode enables the bot to be used in <b>any</b> chat, group or channel – it doesn\'t matter whether the bot is a member or not.\n';
-  response += '<i>(Hint: Keep in mind that inline mode doesn\'t allow embedded commands like direct linking to evolutions!)</i>\n\n';
-  response += '<b>How to use:</b>\n';
-  response += 'Type @TreasureCruiseBot in the message field, then type some keywords. The bot will offer you a list of results. Tap on an item to instantly send it to the chat.\n\n';
-  response += '<b>Example:</b>\n';
+  var response = '<b>I S T R U Z I O N I/b>\n\n';
+  response += '<b>Modalità inline:</b>\n';
+  response += 'La modalità inline abilità il bot ad essere usato in <b>qualsiasi</b> chat, gruppo o canale – non importa se il bot sia un membro o meno.\n';
+  response += '<i>(Consiglio: Tieni a mente che la modalità inline non permette l\'uso di comandi incorporati come i collegamenti direttili alle evoluzioni!)</i>\n\n';
+  response += '<b>Istruzioni:</b>\n';
+  response += 'Scrivi @TreasureCruiseBot, seguito da qualche parola chiave. Il bot ti offrirà una lista di risultati. Tocca uno dei risultati per inviarlo istantaneamente in chat!.\n\n';
+  response += '<b>Esempio:</b>\n';
   response += '@TreasureCruiseBot boa hancock';
   return {
     text: response,
     reply_markup: {
       inline_keyboard: [
         [{
-          text: 'Let\'s try this!',
+          text: 'Proviamo questo!',
           switch_inline_query: 'boa hancock'
         }]
       ]
@@ -96,15 +96,15 @@ function getInline(cmd, arg, message) {
 }
 
 function getCommand(cmd, arg, message) {
-  var response = '<b>H O W   T O   U S E</b>\n\n';
-  response += '<b>Command Mode:</b>\n';
-  response += 'The command mode can be used when talking to the bot directly, or in a group, <b>if</b> the bot is a member and command mode is not disabled.\n\n';
-  response += '<b>How to use:</b>\n';
-  response += 'Send /search and then reply to the following prompt with some keywords. Tap on a result from the list to get the details.\n';
-  response += '<i>(Hint: You can omit "/search" and go straight to sending your keywords when talking to the bot directly!)</i>\n\n';
-  response += '<b>Example:</b>\n';
+  var response = '<b>I S T R U Z I O N I</b>\n\n';
+  response += '<b>Modalità di comando:</b>\n';
+  response += 'La modalità di comando può essere usata parlando al bot direttamente, oppure in un gruppo, <b>se</b> il bot è un membro e la modalità di comando non è disabilitata.\n\n';
+  response += '<b>Istruzioni:</b>\n';
+  response += 'Invia /search e rispondi alla richiesta seguente con alcune parole chiave. Tocca un risultato della lista per ottenerne i dettagli.\n';
+  response += '<i>(Consiglio: Puoi omettere l\'uso di "/search" e inviare direttamente le parole chiave mentre stai parlando con il bot!)</i>\n\n';
+  response += '<b>Esempio:</b>\n';
   response += '/search\n';
-  response += '<i>wait for prompt...</i>\n';
+  response += '<i>aspetta per la richiesta...</i>\n';
   response += 'boa hancock\n\n';
   response += '<b>Shortcuts:</b>\n';
   response += '/boa_hancock\n';
@@ -117,9 +117,9 @@ function getCommand(cmd, arg, message) {
 
 function getFilter(cmd, arg, message) {
   module.status.addRequest('filter');
-  var response = '<b>H O W   T O   U S E</b>\n\n';
-  response += '<b>Filtering:</b>\n';
-  response += 'I have removed filtering for now. My implementation wasn\'t very good and pretty unstable. I am working on a new version with better filtering.';
+  var response = '<b>I S T R U Z I O N I</b>\n\n';
+  response += '<b>Filtraggio:</b>\n';
+  response += 'Ho rimosso il filtro per ora. La mia implementazione non era troppo stabile, purtroppo. Sto lavorando ad una nuova versione con un miglior filtraggio.';
   return {
     text: response,
     chat_id: message.chat.id
@@ -128,12 +128,12 @@ function getFilter(cmd, arg, message) {
 
 function getNotice(cmd, arg, message) {
   module.status.addRequest('notice');
-  var response = '<b>N O T I C E</b>\n\n';
-  response += 'Hi everyone!\n\n';
-  response += 'Former dev (Mr. Trashbytes) isn\'t working on the bot anymore.\n\n';
-  response += 'Due that, the filtering stuff isn\'t a thing right now (since I -Intervención- don\'t know how to make it\).n\n';
-  response += 'By the way, the DB is up to date.\n\n';
-  response += '@Intervencion, September 18, 2016';
+  var response = '<b>N O T I Z I E</b>\n\n';
+  response += 'Salve a tutti!\n\n';
+  response += 'L\'ex sviluppatore (Mr. Trashbytes) non sta lavorando più al bot.\n\n';
+  response += 'A causa di ciò, ho dovuto rinunciare all\'idea di lavorare sul filtraggio, per ora (dal momento in cui Io -Intervención- non so come farlo\).n\n';
+  response += 'Ad ogni modo, il database è aggiornato.\n\n';
+  response += '@Intervencion, 18 Settembre 2016';
   return {
     text: response,
     disable_web_page_preview: true,
@@ -143,14 +143,14 @@ function getNotice(cmd, arg, message) {
 
 function getSupergroup(cmd, arg, message) {
   module.status.addRequest('supergroup');
-  var response = '<b>S U P E R G R O U P</b>\n\n';
-  response += 'Join our supergroup to travel the world of One Piece together!';
+  var response = '<b>S U P E R G R U P P O</b>\n\n';
+  response += 'Unisciti nel nostro supergruppo per viaggiare nel mondo di One Piece insieme!';
   return {
     text: response,
     reply_markup: {
       inline_keyboard: [
         [{
-          text: 'Join now!',
+          text: 'Unisciti ora!',
           url: 'https://telegram.me/joinchat/ABzveEDxZTiNLYTRO5-kQg'
         }]
       ]
@@ -163,7 +163,7 @@ function getSupergroup(cmd, arg, message) {
 function getGithub(cmd, arg, message) {
   module.status.addRequest('github');
   var response = '<b>G I T H U B</b>\n\n';
-  response += 'Check how I have been made';
+  response += 'Controlla come sono stato fatto';
   return {
     text: response,
     reply_markup: {
