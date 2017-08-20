8aaf249fa57c50fc9ae7fdd60cea890ead1eadf5
Jan Raventós <janraven55@gmail.com>
2017-08-20 19:58:10 +0200

    Fixed error in Aliases

--- optcdb_bot/bots/gem-dev/data/aliasesDB.js
+++ optcdb_bot/bots/gem-dev/data/aliasesDB.js
@@ -1,4 +1,4 @@
-modeule.exports = {
+module.exports = {
     1: ['モンキー・D・ルフィ','Monkey D. Luffy'],
     2: ['モンキー・D・ルフィ ゴムゴムの銃(ピストル)','Monkey D. Luffy Gum-Gum Carabine'],
     3: ['モンキー・D・ルフィ ゴムゴムのバズーカ','Monkey D. Luffy Gum-Gum Bazooka'],
