a35d33987010bd06e6175ca7810aec7c94b2329b
Jan Raventós <janraven55@gmail.com>
2020-03-07 20:23:07 +0100

    bye bye beta

--- optcdb_bot/bots/gem-dev/bin/utils/utils.js
+++
@@ -1,3 +0,0 @@
-String.prototype.replaceEntities = function () {
-  return this.replace(/\s*<br>\s*/g, '\n').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/ +(?= )/g, '').replace('&lt;b&gt;','\n<b>').replace('&lt;/b&gt;','</b>');
-};
